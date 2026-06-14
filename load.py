25df3dd30330f1721496af60d2e2352151b8dbfd
jbav91 <jbav91@gmail.com>
2026-06-14 21:44:44 +0100

    Comments to load.py

--- load.py
+++ load.py
@@ -2,14 +2,14 @@ import os
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 
-# Carga las variables de entorno desde el archivo .env
+# .env varibles
 load_dotenv()
 
 def load_to_postgres(df, table_name):
     """
     Loads a Pandas DataFrame into a PostgreSQL database securely.
     """
-    # Leer credenciales desde el entorno virtual (ocultas)
+    # Read credentials from .env
     DB_USER = os.getenv("DB_USER")
     DB_PASS = os.getenv("DB_PASS")
     DB_HOST = os.getenv("DB_HOST")
@@ -20,7 +20,11 @@ def load_to_postgres(df, table_name):
     
     try:
         engine = create_engine(f'postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
+
+        # Ensure missing values are properly converted to SQL NULL
+        df = df.replace({float('nan'): None})
         
+        # Send data to SQL
         df.to_sql(name=table_name, con=engine, if_exists='append', index=False)
         print("Success: Data successfully loaded into PostgreSQL!")
         
